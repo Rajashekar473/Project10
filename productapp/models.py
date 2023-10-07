fbba881686f0e062236a8b9ebaa8faf4229c21bd
Rajashekar473 <131675237+Rajashekar473@users.noreply.github.com>
2023-10-07 12:54:16 +0530

    Update models.py

--- productapp/models.py
+++ productapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):migra
+class Product(models.Model)
     pid=models.IntegerField(primary_key=True)
     pname=models.CharField(max_length=20)
     pcost=models.DecimalField(max_digits=10,decimal_places=2)
